958d3f661bcc1fc490f38cf7548baac3e72c77a3
vairojarela <vairojarela@gmail.com>
2019-10-17 16:23:38 +0200

    Updates

---
+++ precache-manifest.65c59a264c285e3b272c50c57f410796.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6af2cc22928f22299a4487192c1a773b",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "7ed70d8c54b701b81c22",
+    "url": "/personal-website/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "8da21339512630534d38",
+    "url": "/personal-website/static/css/main.4dfba560.chunk.css"
+  },
+  {
+    "revision": "7ed70d8c54b701b81c22",
+    "url": "/personal-website/static/js/2.428f8c47.chunk.js"
+  },
+  {
+    "revision": "8da21339512630534d38",
+    "url": "/personal-website/static/js/main.dcb8aac1.chunk.js"
+  },
+  {
+    "revision": "dc147e1ff0f770db3fbd",
+    "url": "/personal-website/static/js/runtime-main.cfc3c866.js"
+  }
+]);
